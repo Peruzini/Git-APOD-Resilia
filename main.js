901c6a5ffddf136546e24ed9e8cd9ae74d211765
Peruzini <assamitafael@gmail.com>
2022-02-15 21:13:54 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,54 @@
+let data = document.getElementById('data');
+let botao = document.getElementById('botao');
+
+let corpo = document.body;
+
+let container = document.getElementById('container');
+
+const video = document.createElement('iframe');
+
+botao.addEventListener('click',()=>{
+    let url = `https://api.nasa.gov/planetary/apod?api_key=HV9UggNUqbRLwydlP5ersxSk9ivZQLbMDtpC7j8Y&date=${data.value}`
+
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET',url);
+
+    xhr.addEventListener('load', ()=>{
+        let resposta = xhr.responseText;
+        let conversao = JSON.parse(resposta);
+
+        let imagem = conversao.url;
+        
+
+        if(conversao.media_type === 'image'){
+            
+            video.style.display = 'none';
+            corpo.style.backgroundImage = `url(${imagem})`;
+        }else{
+            
+            const caixaVideo = document.createElement('div');
+            caixaVideo.setAttribute('id','box-video');
+            container.append(caixaVideo);
+
+            const texto = document.createElement('h3');
+            texto.setAttribute('id','titulo');
+            texto.textContent = 'Vídeo';
+            caixaVideo.append(texto);
+
+            caixaVideo.append(video);
+            video.setAttribute('id','iframe');
+
+            caixaVideo.style.display = 'flex';
+            caixaVideo.style.flexDirection = 'column'
+            caixaVideo.style.textAlign = 'center'
+
+            video.src = conversao.url;
+            video.style.display = 'block';
+            
+        }
+
+    });
+
+    xhr.send();
+});
